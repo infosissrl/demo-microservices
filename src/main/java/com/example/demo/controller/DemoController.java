b9e9d33b94123ad9b92c739acb0e234d48610604
Razvan Juravle <razvan.juravle@metrosystems.net>
2018-06-23 21:40:58 +0300

    fix controller mapping

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -4,10 +4,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/")
+@RestController
+@RequestMapping(value = "/demo")
 public class DemoController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String sayHello() {
         return "Hello!";
     }
